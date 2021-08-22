80e215fc4cf0a994120ecfb8d5f4890be0210109
Martin Klestil <34619286+mklestil@users.noreply.github.com>
2021-08-22 22:12:49 +0200

    create BasicsProject

---
+++ BasicsProject/main.js
@@ -0,0 +1,30 @@
+//load electron
+const { app, BrowserWindow } = require('electron')
+
+// create window
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+
+        }
+    })
+
+    win.loadFile('index.html')
+}
+// app wide, create window
+app.whenReady().then(createWindow)
+
+// listener, all windows closed, exit app 
+app.on('window-all-closed', () => {
+    app.quit
+})
+
+// listener, if the app run but no window, create window
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows.length === 0){
+        createWindow()
+    }
+})
